fb01f13b1020bf91983c46368d4dd7d00ddc775e
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-03-15 10:22:05 +0100

    Add monkey path for Object class

--- lib/nil_conditional.rb
+++ lib/nil_conditional.rb
@@ -1,5 +1,17 @@
 require "nil_conditional/version"
 
 module NilConditional
-  # Your code goes here...
+
+  class ::Object
+    def method_missing(name, *params)
+      if name =~ /^.*_\?$/
+        original_method = name.to_s.sub(/_\?$/, '').to_sym
+        return send(original_method, *params) if respond_to?(original_method)
+        nil
+      else
+        super
+      end
+    end
+  end
+
 end
