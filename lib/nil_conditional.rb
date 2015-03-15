7e55ad1146022fb774e5560a2c0fc44546655e0f
Grzegorz Bizon <grzegorz.bizon@ntsn.pl>
2015-03-15 12:47:51 +0100

    It should work only with not empty methods (operators are not supported yet)

--- lib/nil_conditional.rb
+++ lib/nil_conditional.rb
@@ -4,7 +4,7 @@ module NilConditional
 
   class ::Object
     def method_missing(name, *params)
-      if name =~ /^.*_\?$/
+      if name =~ /^.+_\?$/
         original_method = name.to_s.sub(/_\?$/, '').to_sym
         return send(original_method, *params) if respond_to?(original_method)
         nil
